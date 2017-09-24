44675031be4ef2461dafbeaf2cd87a7a9164bc68
Caio Frota <caiofrota@gmail.com>
2017-09-23 21:23:34 -0300

    I18n support.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { TranslateService } from '@ngx-translate/core';
+
 import { AdMobPro } from '@ionic-native/admob-pro';
 
 import { MyAppAdMobConfig } from '../providers/app.config';
@@ -15,7 +17,14 @@ export class MyApp {
     rootPage: any = HomeController;
     admobId: any;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, AdMobPro: AdMobPro) {
+    constructor(platform: Platform,
+                statusBar: StatusBar,
+                splashScreen: SplashScreen,
+                translate: TranslateService,
+                AdMobPro: AdMobPro) {
+        translate.setDefaultLang('en');
+        translate.use(navigator.language);
+
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Clipboard } from '@ionic-native/clipboard';
 import { AdMobPro } from '@ionic-native/admob-pro';
 
-import { Clipboard } from '@ionic-native/clipboard';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 
+import { createTranslateLoader } from '../providers/translation-loader';
 import { MyAppFirebaseConfig } from '../providers/app.config';
+
 import { HomeController } from '../pages/home';
 import { ChecklistController, ChecklistService, ChecklistStorageService } from '../pages/checklist';
 
@@ -26,6 +31,14 @@ import { MyApp } from './app.component';
     imports: [
         BrowserModule,
         IonicModule.forRoot(MyApp),
+        HttpModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [Http]
+            }
+        }),
         AngularFireAuthModule,
         AngularFireDatabaseModule,
         AngularFireModule.initializeApp(MyAppFirebaseConfig)
